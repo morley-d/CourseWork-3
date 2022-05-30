6dfe54a6cfd79ec6ab6223276361d732a058f911
San <andreal87@mail.ru>
2022-05-30 15:52:37 +0600

    add posts_dao

---
+++ app/posts/dao/posts_dao.py
@@ -0,0 +1,41 @@
+import json
+
+
+class PostsDAO:
+    """ Класс для загрузки данных для постов """
+
+    def __init__(self, path):
+        self.path = path
+
+    def load(self):
+        with open("data/posts.json", 'r', encoding='utf-8') as file:
+            return json.load(file)
+
+    # возвращает все посты
+    def get_all(self):
+        return self.load()
+
+    # возвращает пост по его идентификатору
+    def get_by_pk(self, pk):
+        posts = self.get_all()
+        for post in posts:
+            if post['pk'] == pk:
+                return post
+
+    # возвращает посты определенного пользователя
+    def get_posts_by_user(self, user_name):
+        posts = self.get_all()
+        posts_founded = []
+        for post in posts:
+            if user_name.lower() in post['poster_name'].lower():
+                posts_founded.append(post)
+        return posts_founded
+
+    # возвращает список словарей по вхождению
+    def search(self, query):
+        posts = self.get_all()
+        matching_posts = []
+        for post in posts:
+            if query.lower() in post['content'].lower():
+                matching_posts.append(post)
+        return matching_posts
