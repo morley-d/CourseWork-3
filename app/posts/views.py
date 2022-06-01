66d9b71a110dee50098a904d2fe3964834e34cdb
San <andreal87@mail.ru>
2022-06-01 10:31:09 +0600

    add logging if def posts_by_users

--- app/posts/views.py
+++ app/posts/views.py
@@ -53,6 +53,7 @@ def posts_search():
 @posts_blueprint.route('/users/<username>/')
 def posts_by_users(username):
     posts = posts_dao.get_posts_by_user(username)
+    logger.debug(f'Запрошены посты пользователя"{username}"')
     return render_template('user-feed.html', username=username, posts=posts)
 
 
